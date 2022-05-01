cfbc1093e932b60e782fcb444524e6e4b962d32c
hai <hai@entest.io>
2022-05-01 13:07:55 +0000

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
-import { AmplifyProvider } from '@aws-amplify/ui-react';
+import { AmplifyProvider, ColorMode } from '@aws-amplify/ui-react';
 import * as React from 'react';
 import '../styles/index.scss';
 import { myTheme } from '../theme';
 import { Header } from '../components/Layout/Header';
+import { useRouter } from 'next/router';
+
 
 function App({ Component, pageProps }) {
+
+  const router = useRouter();
+  const { platform = "react" } = router.query;
+  const [colorMode, setColorMode] = React.useState('system');
   const [themeOverride, setThemeOverride] = React.useState('amplify-docs');
 
   React.useEffect(() => {
@@ -14,11 +20,22 @@ function App({ Component, pageProps }) {
     );
   }, [themeOverride]);
 
+
   return (
-    <AmplifyProvider theme={myTheme}>
-      <Header ></Header>
-      <Component {...pageProps}></Component>
-    </AmplifyProvider>
+    <>
+      <AmplifyProvider theme={myTheme} colorMode={colorMode as ColorMode}>
+        <Header
+          platform={platform}
+          colorMode={colorMode}
+          setColorMode={setColorMode}></Header>
+        <Component
+          {...pageProps}
+          colorMode={colorMode}
+          setThemeOverride={setThemeOverride}
+          themeOverride={themeOverride}
+        ></Component>
+      </AmplifyProvider>
+    </>
   );
 }
 
